ef8f9196f7110b2d67449c8c6e1b9938e1757b85
Konstantin8105 <konstantin8105@gmail.com>
2018-09-22 00:09:16 +0300

    add fail test with singular error

--- lis_test.go
+++ lis_test.go
@@ -22,6 +22,23 @@ func TestLsolve(t *testing.T) {
 	fmt.Println(s, r, o, err)
 }
 
+func TestLsolveSingular(t *testing.T) {
+	A := mat.NewDense(2, 2, []float64{
+		1.0, 2.0,
+		1.e-30, 1e-30,
+	})
+	b := mat.NewDense(2, 1, []float64{
+		3.0,
+		2.0e-30,
+	})
+
+	s, r, o, err := golis.Lsolve(A, b, 0)
+	fmt.Println(s, r, o, err)
+	if err == nil {
+		t.Fatalf("Haven`t error : %v", err)
+	}
+}
+
 func TestLsolveFail(t *testing.T) {
 	A := mat.NewDense(2, 2, []float64{
 		1.0, 2.0,
